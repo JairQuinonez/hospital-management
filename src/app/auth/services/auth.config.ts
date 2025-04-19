b73a69c3867f7c14a98f34994db0a8987d32344a
Jair Quinonez <jair.quinonez@sofka.com.co>
2025-04-19 00:59:05 -0500

    credential clear

--- src/app/auth/services/auth.config.ts
+++ src/app/auth/services/auth.config.ts
@@ -4,10 +4,10 @@ export const authCodeFlowConfig: AuthConfig = {
     issuer: 'https://accounts.google.com',
     strictDiscoveryDocumentValidation: false,
     redirectUri: window.location.origin+'/role-selection',
-    clientId: '22704243115-do7761urr84aka6r54h4d9f4o47nq0id.apps.googleusercontent.com',
+    clientId: '',
     scope: 'openid profile email ',
     showDebugInformation: true,
     responseType: 'code',
     oidc: true,
-    dummyClientSecret: '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'
+    dummyClientSecret: ''
   };
